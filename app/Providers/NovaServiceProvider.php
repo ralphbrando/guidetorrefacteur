fea6cd9df644daab8c6916315f60ad1fcb148c8a
Ralph Brando <admin@ralphbrando.com>
2025-12-04 14:48:18 +0100

    f

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -42,7 +42,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     {
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                // Add admin emails here
+                'appdevxxx@gmail.com',
             ]) || $user->isAdmin();
         });
     }
